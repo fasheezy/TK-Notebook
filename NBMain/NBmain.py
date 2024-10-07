27b719765165ab09f2f7da85637dd478a969cbf6
JT Coder <53548183+fasheezy@users.noreply.github.com>
2024-10-07 01:35:57 -0400

    Add files via upload

---
+++ NBMain/NBmain.py
@@ -0,0 +1,231 @@
+import tkinter as tk
+import json
+import os
+from tkinter import filedialog,messagebox,font, Tk,ttk,scrolledtext
+#from sympy import sympify,SympifyError
+from tkinterdnd2 import TkinterDnD, DND_FILES
+from auxiliary import * 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import shutil 
+
+class Application(TkinterDnD.Tk):
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        self.title("No Save Loaded")
+        self.geometry("600x400")
+        self.current_file = None
+        self.defaultFont = font.nametofont("TkDefaultFont")
+        self.defaultFont.configure(family="Times New Roman")
+        self.restarted = True
+        self.entry_boxes = {}
+        self.info_boxes = {}
+        self.pic_boxes={}
+        self.label_num = -1
+        self.row_counter = 0 
+
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(side=tk.LEFT,fill=tk.Y)
+        self.current_file=None
+        self.save_directory = "saved_states"
+        self.holding_files = "image_files"
+        self.image_depot = "image_dump"
+        os.makedirs(self.save_directory, exist_ok=True)
+        os.makedirs(self.image_depot, exist_ok=True)
+        os.makedirs(self.holding_files,exist_ok=True)
+
+        self.main_canvas = tk.Canvas(self)
+        self.main_canvas.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+        self.frame_1 = tk.Frame(self.main_canvas)
+
+        self.main_canvas.create_window((4,4),window = self.frame_1,anchor="nw")
+        self.frame_1.bind("<Configure>", self.on_configure)
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.main_canvas.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.main_canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.button0 = tk.Button(self.button_frame,text="Reload Old Save",command=self.reload_state)
+        self.button1 = tk.Button(self.button_frame,text="Create Text Boxes",command = self.make_new_entry)
+        self.button2 = tk.Button(self.button_frame,text="Create Graph",command=self.create_graphs)
+        self.button3 = tk.Button(self.button_frame,text="Create Image Box",command=self.image_placer)
+        self.button4 = tk.Button(self.button_frame,text="Create New Save",command=self.create_new_state)
+        self.button0.pack(pady=5,fill="x")
+        self.button1.pack(pady=5,fill="x")
+        self.button2.pack(pady=5,fill="x")
+        self.button3.pack(pady=5,fill="x")
+        self.button4.pack(pady=5,fill="x")
+    def on_configure(self,event):
+        self.main_canvas.configure(scrollregion=self.main_canvas.bbox("all"))
+    def make_labels(self):
+        self.label_num+=2
+        self.entry_boxes["Entry"+str(self.label_num)] = tk.Entry(self.frame_1,fg='gray1')
+        if self.restarted==True:
+            self.entry_boxes["Entry"+str(self.label_num)].insert(0,"Create A Label")
+        backdoor = backend(self.entry_boxes["Entry"+str(self.label_num)],self.label_num)
+        button = tk.Button(self.frame_1,text ="Delete Entry",command = lambda: self.remove_widget(button))
+        button.grid(row=self.label_num,column=1,sticky="e")
+        backdoor.boxcreate()
+
+    def make_new_entry(self):
+        if self.restarted==True:
+            self.make_labels()
+        self.row_counter+=2
+        self.info_boxes["infobox"+str(self.row_counter)] = scrolledtext.ScrolledText(self.frame_1,wrap=tk.WORD,width=55,height=7)
+        boxes = backend(self.info_boxes["infobox"+str(self.row_counter)],self.row_counter)
+        boxes.entrymake()
+    def image_placer(self):
+        if self.restarted==True:
+            self.make_labels()
+        self.row_counter+=2
+        self.pic_boxes["picbox"+str(self.row_counter)] = tk.Label(self.frame_1, text="Drag and drop an image here", bg="gray", width=40, height=20)
+        boxes = backend(self.pic_boxes["picbox"+str(self.row_counter)],self.row_counter)
+        boxes.imageshow()
+    def remove_widget(self,loc):
+        extract = loc.grid_info()["row"]
+        for widget in self.frame_1.grid_slaves(row=extract, column=1):
+            widget.grid_remove()  
+        for widget in self.frame_1.grid_slaves(row=extract+1,column=1):
+            widget.grid_remove()  
+            if isinstance(widget,tk.Label):
+                self.pic_boxes.pop("picbox"+str(extract+1))
+                for photo in os.listdir("image_dump"):
+                    if extract+1 == int(photo.split("_")[1][0]):
+                        os.remove("image_dump/"+photo)
+            elif isinstance(widget,tk.Entry):
+                self.entry_boxes.pop("Entry"+str(extract+1))
+            elif isinstance(widget,tk.Frame):
+                self.info_boxes.pop("infobox"+str(extract+1))
+
+        self.row_counter -=2
+        self.label_num -=2
+    def create_graphs(self):
+        helper = iterfuncs()
+        self.row_counter +=2
+        self.label_num +=2 
+        button = tk.Button(self.frame_1,text ="Delete Entry",command = lambda: self.remove_widget(button))
+        button.grid(row=self.label_num,column=1,sticky="e")
+        fig, ax = plt.subplots(figsize=(4, 2))
+        canvas = FigureCanvasTkAgg(fig, master=self.frame_1) 
+        self.entry_boxes["Entry"+str(self.label_num)] = tk.Entry(self.frame_1,fg='gray2')
+        graphix = plotdow(ax,canvas,fig)
+        canvas.get_tk_widget().grid(row=self.row_counter,column=1)
+        self.entry_boxes["Entry"+str(self.label_num)].bind("<KeyRelease>",graphix.plot_graph)
+        self.entry_boxes["Entry"+str(self.label_num)].bind("<FocusIn>", helper.clear_graph)
+        self.entry_boxes["Entry"+str(self.label_num)].bind("<FocusOut>",helper.add_graph)
+        canvas.get_tk_widget().bind("<Button-4>", graphix.zoom_in)  
+        canvas.get_tk_widget().bind("<Button-5>", graphix.zoom_out)  
+        canvas.get_tk_widget().bind("<MouseWheel>", graphix.zoom) 
+        if self.restarted==True:
+            self.entry_boxes["Entry"+str(self.label_num)].insert(0,"Enter an Equation")
+        self.entry_boxes["Entry"+str(self.label_num)].grid(sticky="w",row=self.label_num,column=1)
+    def create_new_state(self):
+        master = {}
+        club = ""
+        row = ""
+        photo_list = []
+        textlocs = []
+        photo_num = 0
+        fix = iterfuncs()
+        new_state_name = filedialog.asksaveasfilename(
+            initialdir=self.save_directory,
+            defaultextension=".json",
+            filetypes=[("JSON", "*.json"), ("All Files", "*.*")],
+            title="Create New State")
+        if new_state_name:
+            file_name = new_state_name.split("/")[-1]
+            try:
+                os.mkdir("image_files/"+file_name[:-5]) 
+            except: 
+                pass
+        for images in os.listdir("image_dump"):
+            photo_list.append(images)
+            try:
+                shutil.move("image_dump/"+images,"image_files/"+file_name[:-5])
+            except:
+                pass
+        master["Title"] = file_name[:-5]
+        for widget in self.frame_1.winfo_children():
+            club = widget.grid_info()
+            row = club["row"]
+            if isinstance(widget,tk.Entry): 
+                color = widget.cget("foreground")  
+                master["Entry"+color+"_"+str(row)] = widget.get()
+            elif isinstance(widget, tk.Frame):
+                for child in widget.winfo_children():
+                    if isinstance(child,tk.Text):   
+                        tick = child.get('1.0', tk.END)
+                        extratags = fix.set_tags(child)
+                        text = fix.replace_back(tick)
+                        textlocs.append(text)
+                        del extratags["sel"]
+                        textlocs.append(extratags)
+                        master["Text_"+str(row)] = textlocs
+                    textlocs = []
+            elif isinstance(widget,tk.Label):
+                master["Photo_"+str(row)] = "image_files/"+file_name[:-5]+ "/"+photo_list[photo_num]   
+                photo_num+=1
+        leak = open(new_state_name,"w")
+        json.dump(master,leak,indent=1)
+        self.current_file = new_state_name
+        self.reset_widgets()
+        
+       
+    def reload_state(self):
+        function = iterfuncs()
+        self.restarted= False
+        self.row_counter = 0
+        self.label_num = -1
+        old_state_name = filedialog.askopenfile(mode='r', filetypes=[("JSON files", "*.json")])
+        dictims = json.load(old_state_name)
+        n = 0
+        for key,value in dictims.items():
+            n+=1
+            if "Title" in key:
+                self.title(value)
+                n -=1
+            if "Entrygray1" in key:
+                self.make_labels()
+                self.entry_boxes["Entry"+str(n)].insert(0,value)
+                
+            elif "Text" in key:
+                self.make_new_entry()
+                self.info_boxes["infobox"+str(n)].insert('1.0',value[0])
+                function.re_tag(self.info_boxes["infobox"+str(n)],value[1])
+            elif "Photo" in key:
+                shutil.copy(value,"image_dump")
+                self.image_placer()
+                imager = Image.open(value)
+                image = imager.resize((600,500))
+                self.image_tk = ImageTk.PhotoImage(image)
+                self.pic_boxes["picbox"+str(n)].config(image=self.image_tk, text='')
+                self.pic_boxes["picbox"+str(n)].config(width=self.image_tk.width(), height=self.image_tk.height())
+            elif "Entrygray2" in key:
+                self.create_graphs()
+                self.entry_boxes["Entry"+str(n)].insert(0,value)
+                n+=1
+        self.restarted=True
+
+
+
+
+    def reset_widgets(self):
+        for widget in self.frame_1.winfo_children():
+            widget.destroy()
+        self.entry_boxes = {}
+        self.infob_info = {}
+        self.pic_boxes={}
+    def on_closing(self):
+        try:
+            self.create_new_state()
+        except:
+            pass
+        for file in os.listdir("image_dump"):
+            os.remove("image_dump/"+file)
+        self.is_closing = True  
+        self.quit()
+if __name__ == "__main__":
+    app = Application()
+    app.protocol("WM_DELETE_WINDOW", app.on_closing)
+    app.mainloop()
---
+++ NBMain/auxiliary.py
@@ -0,0 +1,247 @@
+import tkinter as tk
+from tkinter import messagebox
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinterdnd2 import TkinterDnD, DND_FILES
+from PIL import Image, ImageTk
+import os
+import shutil
+def aroundtwice(input1):
+    used = input1.split(".")[1]
+    use = int(used) -1
+    result = str(input1.split(".")[0])+"."+str(use)
+    return result
+class backend:
+    def __init__(self,entry,counter):
+        self.entry = entry 
+        self.counter = counter
+        self.enforce = iterfuncs()
+    def boxcreate(self):
+        self.entry.grid(row = self.counter,column=1,sticky="w")
+        self.entry.bind("<FocusIn>",self.enforce.clear_placeholder)
+        self.entry.bind("<FocusOut>",self.enforce.add_placeholder)
+        self.entry.bind("<Return>",self.enforce.set_labellers)
+    def entrymake(self):
+        self.entry.grid(row=self.counter,column=1,pady=3,sticky="w")
+        self.entry.bind("<KeyPress>",self.enforce.delete_char)
+        self.entry.bind('<<Modified>>', self.enforce.check_for_special)
+    def imageshow(self):
+        self.entry.grid(row=self.counter,column=1,pady=3,sticky="w")
+        self.entry.drop_target_register(DND_FILES)
+        self.entry.dnd_bind('<<Drop>>', self.enforce.drop_image)
+
+class iterfuncs:
+    conversions = {
+        '/alpha': 'α', '/beta': 'β', '/gamma': 'γ', '/delta': 'δ',
+        '/epsilon': 'ε', '/zeta': 'ζ', '/eta': 'η', '/theta': 'θ',
+        '/iota': 'ι', '/kappa': 'κ', '/lambda': 'λ', '/mu': 'μ',
+        '/nu': 'ν', '/xi': 'ξ', '/omicron': 'ο', '/pi': 'π',
+        '/rho': 'ρ', '/sigma': 'σ', '/tau': 'τ', '/upsilon': 'υ',
+        '/phi': 'φ', '/chi': 'χ', '/psi': 'ψ', '/omega': 'ω',
+        '/Alpha': 'Α', '/Beta': 'Β', '/Gamma': 'Γ', '/Delta': 'Δ',
+        '/Epsilon': 'Ε', '/Zeta': 'Ζ', '/Eta': 'Η', '/Theta': 'Θ',
+        '/Iota': 'Ι', '/Kappa': 'Κ', '/Lambda': 'Λ', '/Mu': 'Μ',
+        '/Nu': 'Ν', '/Xi': 'Ξ', '/Omicron': 'Ο', '/Pi': 'Π',
+        '/Rho': 'Ρ', '/Sigma': 'Σ', '/Tau': 'Τ', '/Upsilon': 'Υ',
+        '/Phi': 'Φ', '/Chi': 'Χ', '/Psi': 'Ψ', '/Omega': 'Ω',
+        '/infty': '∞', '/sqrt': '√', '/sum': '∑', '/prod': '∏',
+        '/int': '∫', '/partial': '∂', '/nabla': '∇', '/forall': '∀',
+        '/exists': '∃', '/neg': '¬', '/rightarrow': '→', '/leftarrow': '←',
+        '/leftrightarrow': '↔', '/uparrow': '↑', '/downarrow': '↓',
+        '/pm': '±', '/times': '×', '/div': '÷', '/neq': '≠',
+        '/approx': '≈', '/leq': '≤', '/geq': '≥', '/subset': '⊂',
+        '/supset': '⊃', '/subseteq': '⊆', '/supseteq': '⊇', '/cup': '∪',
+        '/cap': '∩', '/emptyset': '∅', '/In': '∈', '/notin': '∉',
+        '/angle': '∠', '/perp': '⊥', '/cdot': '⋅', '/star': '★',
+        '/propto': '∝', '/equiv': '≡', '/otimes': '⊗', '/oplus': '⊕',
+        '/bullet': '•', '/dagger': '†', '/ddagger': '‡', '/aleph': 'ℵ',
+        '/prime': '′', '/hbar': 'ℏ', '/ell': 'ℓ', '/Re': 'ℜ',
+        '/Im': 'ℑ', '/wp': '℘', '/degree': '°', '/copyright': '©',
+        '/registered': '®', '/paragraph': '¶', '/section': '§',
+        '/therefore': '∴', '/because': '∵', '/angle': '∠', '/triangle': '△',
+        '/lozenge': '◊', '/clubsuit': '♣', '/diamondsuit': '♦', '/heartsuit': '♥',
+        '/spadesuit': '♠', '/male': '♂', '/female': '♀'}
+    def __init__(self):
+        self.notations = ["noscript","subscript","superscript"]
+        self.tagnum = 0 
+        self.used_tag = self.notations[self.tagnum]
+        self.loc = []
+    def clear_graph(self,event):
+        if event.widget.get() == "Enter an Equation":
+            event.widget.delete(0, tk.END)
+    def add_graph(self,event):
+        if event.widget.get() == "":
+            event.widget.insert(0,"Enter an Equation")        
+    def clear_placeholder(self, event):
+        if event.widget.get()== "Create A Label":
+            event.widget.delete(0, tk.END)
+    def add_placeholder(self, event):
+        if event.widget.get() == "":
+            event.widget.insert(0,"Create A Label")
+    def drop_image(self, event):
+        file_path = event.data.strip('{}')
+        print(file_path)
+        try:
+            imager = Image.open(file_path)
+            image = imager.resize((600,500))
+            self.image_tk = ImageTk.PhotoImage(image)
+            event.widget.config(image=self.image_tk, text='')
+            event.widget.config(width=self.image_tk.width(), height=self.image_tk.height())
+        except Exception as e:
+            print(f"Error opening image: {e}")
+        ext = os.path.splitext(file_path)[1]
+        new_filename = "photo_" +str(event.widget.grid_info()["row"]) + ext
+        target_path = os.path.join("image_dump",new_filename) 
+        try:
+            shutil.copy(file_path, target_path)
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to move the image: {e}")
+    def set_labellers(self,event):
+        if event.widget.get() != "":
+            self.new_stuff = event.widget.get()
+            event.widget.delete(0, tk.END)
+            self.focus_set()
+            event.widget.insert(0,self.new_stuff)
+            self.entry_info[event.widget] = self.new_stuff
+    def check_for_special(self,event=None):
+        event.widget.tag_configure("superscript", offset=8, font=("Times New Roman", 10))
+        event.widget.tag_configure("subscript", offset=-4, font=("Times New Roman", 10))
+        event.widget.tag_configure("noscript",offset=0,font=("Times New Roman", 13))
+        if event.widget.edit_modified():
+            event.widget.edit_modified(False)
+            for word_to_replace,replacement_word in self.conversions.items():
+                start_pos = '1.0'  
+                replace_lambda = lambda start_pos: (event.widget.search(word_to_replace, start_pos, stopindex=tk.END))
+                while True:
+                    start_pos = replace_lambda(start_pos)
+                    if not start_pos:
+                        break  
+                    end_pos = f"{start_pos}+{len(word_to_replace)}c"
+                    event.widget.delete(start_pos, end_pos)
+                    event.widget.insert(start_pos, replacement_word)
+                    
+                    start_pos = end_pos
+            lp = event.widget.index(tk.INSERT)        
+            event.widget.tag_add(self.used_tag,f"{lp}-1c")
+    def replace_back(self,insert):
+        entries = insert
+        for unspecial,special in self.conversions.items():
+            entries = entries.replace(special,unspecial)
+        return entries
+
+    def delete_char(self,event):
+        if event.keysym in ("Left", "Right", "Up", "Down", "Home", "End", "BackSpace", "Delete"):
+            return None 
+        if event.char=="`": 
+            self.tagnum = (self.tagnum+1) %len(self.notations)
+            self.used_tag = self.notations[self.tagnum]
+            return "break"
+        elif self.used_tag:
+            event.widget.insert(tk.INSERT,event.char)
+            return "break"
+
+        return None
+    def set_tags(self,item):
+        mainlist = {}
+        templist = []
+        all_tags = item.tag_names()
+        if all_tags:
+            for tag in all_tags:
+                ranges = item.tag_ranges(tag)
+                if ranges:
+                    for i in range(0,len(ranges),2):
+                        templist.append([f'{ranges[i]}',f'{ranges[i+1]}'])
+                mainlist[tag] = templist
+                templist = []
+        return mainlist
+    def re_tag(self,widget,dictionary):
+        widget.tag_configure("superscript", offset=8, font=("Times New Roman", 10))
+        widget.tag_configure("subscript", offset=-4, font=("Times New Roman", 10))
+        widget.tag_configure("noscript",offset=0,font=("Times New Roman", 13))
+        for key,value in dictionary.items():
+            for rows in value:
+                widget.tag_add(key, rows[0], rows[1])
+
+class plotdow:
+    def __init__(self, ax,canvas,fig):
+        self.ax = ax 
+        self.canvas = canvas 
+        self.fig = fig 
+        self.xlim = [-10, 10]
+        self.ylim = [-2, 2]
+        self.ax.set_xlim(self.xlim)
+        self.ax.set_ylim(self.ylim)
+        self.equation = ""
+    def plot_graph(self,event):
+        equation = event.widget.get()
+        x = np.linspace(-10, 10, 400)
+        try:
+            if not equation.startswith('y =') and not equation.startswith('y='):
+                pass
+            equation = equation.replace('y=', 'y =')
+            equation = equation.replace('y =', '').strip()
+            equation = equation.replace('^',"**")
+            equation = equation.replace(" x", "x")
+            equation = equation.replace("x", "*x")
+            if equation.startswith("*x"):
+                equation = equation.replace("*x", "x")
+            equation = equation.replace("(*x","(x")
+            allowed_functions = {"np": np, "sin": np.sin, "cos": np.cos, "tan": np.tan, "x": x}
+            y = eval(equation, {"__builtins__": None}, allowed_functions)
+            if isinstance(y, (int, float)):
+                y = np.full_like(x, y)
+            self.ax.clear()
+            self.ax.set_xlabel('x')
+            self.ax.set_ylabel('y')
+            self.ax.grid(True)
+            self.canvas.draw()
+            self.equation = equation
+        except Exception as e:
+            self.ax.clear()
+            self.ax.set_title('Invalid Equation')
+            self.canvas.draw()
+    def draw_axes(self):
+        self.ax.axhline(0, color='black', linewidth=1)  
+        self.ax.axvline(0, color='black', linewidth=1) 
+
+    def zoom(self, event):
+        if event.delta > 0:
+            self.zoom_in(event)
+        elif event.delta < 0:
+            self.zoom_out(event)
+
+    def zoom_in(self,event):
+        self.xlim = [self.xlim[0] * 0.9, self.xlim[1] * 0.9] 
+        self.ylim = [self.ylim[0] * 0.9, self.ylim[1] * 0.9]  
+        self.update_plot()
+
+    def zoom_out(self,event):
+        self.xlim = [self.xlim[0] * 1.1, self.xlim[1] * 1.1] 
+        self.ylim = [self.ylim[0] * 1.1, self.ylim[1] * 1.1]  
+        self.update_plot()
+
+    def update_plot(self):
+        self.ax.cla() 
+        self.ax.grid(True) 
+        self.ax.set_xlim(self.xlim)
+        self.ax.set_ylim(self.ylim)
+        self.draw_axes()
+        self.x = np.linspace(self.xlim[0], self.xlim[1], 1000)
+        self.update_function()
+
+    def update_function(self):
+        try:
+            equation = self.equation
+            allowed_functions = {"np": np, "sin": np.sin, "cos": np.cos, "tan": np.tan, "x": self.x}
+            self.y = eval(equation, {"__builtins__": None}, allowed_functions)
+            self.y = eval(equation, {"x": self.x, "np": np,"__builtins__": None},allowed_functions)
+            self.ax.set_title(f'Graph of y = {equation}')
+            self.plot, = self.ax.plot(self.x, self.y)
+            self.ax.grid(True)
+            self.canvas.draw()
+        except Exception as e:
+            print(f"Error in equation: {e}")
+
+
+        
